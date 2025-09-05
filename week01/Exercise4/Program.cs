5d4b606f11af5d1a040696f238bdbaf48c42f4fd
AbelChiwandire <achiwandire1@byupathway.edu>
2025-09-05 18:52:00 +0200

    loops exercise project

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,39 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        while (true)
+        {
+            Console.Write("Enter number: ");
+            int number = int.Parse(Console.ReadLine());
+            if (number == 0)
+                break;
+            numbers.Add(number);
+        }
+        int sum = 0;
+        foreach (int num in numbers)
+        {
+            sum += num;
+        }
+        Console.WriteLine($"The sum is: {sum}");
+
+        double average = (double)sum / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        int largest_num = numbers[0];
+        foreach (int num in numbers)
+        {
+            if (num > largest_num)
+            {
+                largest_num = num;
+            }
+        }
+        Console.WriteLine($"The largest number is: {largest_num}");
     }
 }

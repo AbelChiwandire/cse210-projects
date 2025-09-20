2c1b5f62af125af6039b942246fe8ad516f1ca0f
AbelChiwandire <achiwandire1@byupathway.edu>
2025-09-20 21:20:33 +0200

    Scripture memorizer fist commit

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ScriptureMemorizer Project.");
-    }
+        Reference reference = new Reference("John", 3, 16);
+        Scripture scripture = new Scripture(reference, "For God so loved the world that He gave His only Son.");
+
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine(scripture.GetDisplayText());
+
+            if (scripture.IsCompletelyHidden())
+                break;
+
+            Console.WriteLine("\nPress Enter to continue or type 'hint' to reveal some hidden words or type 'quit' to exit.");
+            string input = Console.ReadLine().Trim().ToLower();
+            switch (input)
+            {
+                case "quit":
+                    return;
+                case "hint":
+                    scripture.RevealWords(2);
+                    break;
+                default:
+                    scripture.HideRandomWords(2);
+                    break;
+            }
+        }
+    }           
 }
---
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -0,0 +1,84 @@
+using System;
+using System.Security.Cryptography.X509Certificates;
+
+public class Scripture
+{
+    private Reference _reference;
+    private List<Word> _words;
+    public Scripture(Reference reference, string text)
+    {
+        _reference = reference;
+        _words = new List<Word>();
+
+        string[] wordArray = text.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        foreach (string word in wordArray)
+        {
+            _words.Add(new Word(word));
+        }
+    }
+
+    public void HideRandomWords(int count)
+    {
+        List<int> visibleIndexes = new List<int>();
+        for (int i = 0; i < _words.Count; i++)
+        {
+            if (!_words[i].IsHidden())
+                visibleIndexes.Add(i);
+        }
+        Random rand = new Random();
+        for (int i = 0; i < count && visibleIndexes.Count > 0; i++)
+        {
+            int randPosition = rand.Next(visibleIndexes.Count); 
+            int wordIndex = visibleIndexes[randPosition];      
+            _words[wordIndex].Hide();                      
+
+            visibleIndexes.RemoveAt(randPosition);           
+        }
+    }
+
+    public void RevealWords(int count)
+    {
+        List<int> hiddenIndexes = new List<int>();
+        for (int i = 0; i < _words.Count; i++)
+        {
+            if (_words[i].IsHidden())
+                hiddenIndexes.Add(i);
+        }
+
+        Random rand = new Random();
+        for (int i = 0; i < count && hiddenIndexes.Count > 0; i++)
+        {
+            int randPosition = rand.Next(hiddenIndexes.Count);
+            int wordIndex = hiddenIndexes[randPosition];
+            _words[wordIndex].Show(); 
+
+            hiddenIndexes.RemoveAt(randPosition);         
+        }
+    }
+
+    public string GetDisplayText()
+    {
+        string referenceText = _reference.GetDisplayText();
+        string wordsText = "";
+
+        for (int i = 0; i < _words.Count; i++)
+        {
+            wordsText += _words[i].GetDisplayText() + " ";
+        }
+        wordsText.TrimEnd();
+
+        return referenceText + " " + wordsText;
+    }
+
+    public bool IsCompletelyHidden()
+    {
+        foreach (var word in _words)
+        {
+            if (!word.IsHidden())
+            {
+                return false; 
+            }
+        }
+        return true; 
+    }
+}

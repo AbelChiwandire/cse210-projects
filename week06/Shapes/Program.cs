708a95e0bb7a22a9c313b000fc2a07b5dd0096bd
AbelChiwandire <achiwandire1@byupathway.edu>
2025-10-07 11:25:30 +0200

    Polymorphism practice

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = new List<Shape>();
+
+        Square s1 = new Square("black", 3);
+        shapes.Add(s1);
+
+        Square s2 = new Square("white", 5.5);
+        shapes.Add(s2);
+
+        Rectangle s3 = new Rectangle("black", 7, 2);
+        shapes.Add(s3);
+
+        Rectangle s4 = new Rectangle("white", 9, 3);
+        shapes.Add(s4);
+
+        Rectangle s5 = new Rectangle("maroon", 10, 2);
+        shapes.Add(s5);
+
+        Circle s6 = new Circle("blue", 4);
+        shapes.Add(s6);
+
+        Circle s7 = new Circle("yellow", 7);
+        shapes.Add(s7);
+
+        foreach (var shape in shapes)
+        {
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+            Console.WriteLine($"{shape} => Color:{color}, Area:{area}");
+        }
     }
 }
